ad999609366b0591b03ab7d13f4ec30bcc156673
Jeff <jeff.winandy@education.lu>
2021-02-09 12:00:57 +0100

    Initial commit

---
+++ FunctionInterpolationApproximator.py
@@ -0,0 +1,142 @@
+import numpy as np
+import scipy.interpolate as spi
+import os
+import pandas as pd
+
+
+class FunctionInterpolationApproximator(object):
+
+    def __init__(self, racines, extrema, domaine,step = 0.05):
+        self.domaine = np.arange(domaine[0], domaine[1]+step, step)
+        self.racines = racines
+        self.extrema = extrema
+
+    def racinesPentes(self):
+        res = []
+        s_e = sorted(self.extrema, key=lambda x: x[0])
+        for r in sorted(self.racines):
+            for i in range(len(self.extrema) - 1):
+                if s_e[i + 1][0] > r:
+                    pente = (s_e[i + 1][1] - s_e[i][1]) / (s_e[i + 1][0] - s_e[i][0])
+                    left = float(r - s_e[i][0])
+                    right = float(s_e[i + 1][0] - r)
+                    if left > right:
+                        factor = left / right
+                    else:
+                        factor = right / left
+                    res.append(factor * pente)
+                    break
+
+        return res
+
+    def derivesPentes(self):
+        rs = list(zip(sorted(self.racines), self.racinesPentes())) + list(
+            zip([e[0] for e in self.extrema], np.zeros(len(self.extrema))))
+        return np.array(sorted(rs, key=lambda x: x[0]))[:, 1]
+
+    def approximate(self):
+        x_y_s = list(zip(self.racines, np.zeros(len(self.racines)))) + self.extrema
+        x_y_s = np.array(sorted(x_y_s, key=lambda x: x[0]))
+        xs = x_y_s[:, 0]
+        ys = x_y_s[:, 1]
+        dp = self.derivesPentes()
+        self.spline = spi.CubicHermiteSpline(xs, ys, dp)
+
+    @property
+    def xs(self):
+        return self.domaine
+
+    @property
+    def ys(self):
+        return list(map(lambda x:self.spline(x),self.domaine))
+
+    @property
+    def min_x(self):
+        return self.domaine[0]
+
+    @property
+    def max_x(self):
+        return self.domaine[-1]
+
+    @property
+    def min_y(self):
+        return min(self.ys)
+
+    @property
+    def max_y(self):
+        return max(self.ys)
+
+    def printPoints(self, separator=""):
+        return separator.join(["({},{})".format(np.around(x, 3),np.around(self.spline(x), 3)) for x in self.domaine])
+
+    def axisText(self):
+        return "\\addplot[smooth,blue,samples=100] coordinates{" + self.printPoints() + "};"
+
+    def geogebraText(self):
+        pts = ",".join([str(np.around(self.spline(x), 3)) for x in self.domaine])
+        return "Function[{" + str(np.around(self.domaine[0],3)) + "," + str(np.around(self.domaine[-1],3)) + "," + pts + "}]"
+
+    def generateTikzPicture(self):
+        with open("picture.tex","w") as texfile:
+            texfile.write(
+                """
+                \\documentclass{standalone}\n
+                \\usepackage{tikz}\n
+                \\usepackage{pgfplots}\n
+                
+                \\pgfplotsset{
+                    standardaxis2/.style={
+                        enlarge x limits=0.15,
+                        enlarge y limits=0.15,
+                        %every axis x label/.style={at={(current axis.right of origin)},anchor=north west},
+                        every axis y label/.style={at={(current axis.above origin)},anchor=north east},
+                        axis lines=middle,
+                        axis line style={-stealth,very thick},
+                        xmin=-10.5,xmax=10.5,ymin=-10.5,ymax=10.5,
+                        xtick distance=1,
+                        ytick distance=1,
+                        extra x ticks={0},
+                        extra x tick style={grid=major, grid style={black},xticklabel style={inner sep=4pt,anchor=north east},xtick style={black}},
+                        xticklabel style={font=\\large, anchor = north, inner sep = 4pt},
+                        yticklabel style={font=\\large, anchor = east, inner sep = 4pt},
+                        xlabel=$x$,
+                        ylabel=$y$,
+                        xlabel style={anchor = south east, inner sep = 4pt},
+                        ylabel style={anchor = north west, inner sep = 4pt},
+                        grid=major,
+                        grid style={thin,densely dotted,black!20}
+                        %tick label style = {font=\\large}
+                }}
+                
+                \\begin{document}\n
+                \\begin{tikzpicture}\n
+                \\begin{axis}[
+                    unit vector ratio*=1 1 1,
+                    standardaxis2,
+                    scale only axis,]\n
+                """
+                + self.axisText() +
+                """
+                \n
+                \\end{axis}\n
+                \\end{tikzpicture}\n
+                \\end{document}
+                
+                """
+            )
+        os.system("pdflatex picture.tex")
+
+
+if __name__ == "__main__":
+    with open("input.csv") as inputFile:
+        df = pd.read_csv(inputFile)
+        sp = [float(p) for p in df.loc[0,"startpoint"].strip("()").split(",")]
+        ep = [float(p) for p in df.loc[0,"endpoint"].strip("()").split(",")]
+        domaine = [sp[0], ep[0]]
+        racines = [float(p) for p in df.loc[0,"racines"].split(" ")]
+        extrema = [sp] + [[float(p) for p in e.strip("()").split(",")]for e in df.loc[0,"extrema"].split(" ")] + [ep]
+        f = FunctionInterpolationApproximator(racines, extrema, domaine)
+        f.approximate()
+        print(f.axisText())
+        print(f.geogebraText())
+        f.generateTikzPicture()
---
+++ app/routes.py
@@ -0,0 +1,168 @@
+import uuid
+
+from flask import render_template, flash, redirect, url_for, request, session
+
+from FunctionInterpolationApproximator import FunctionInterpolationApproximator
+from app import app
+
+import random
+import pickle
+
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+import numpy as np
+from threading import Thread
+from time import time
+
+session_instances = {}
+session_alive = {}
+
+def setDefaultValues():
+    sp = (-12,5)
+    ep = (12,5)
+    racines = [-10,-4,-1,2,5,9]
+    extrema = [(-8,-3),(-6,-1),(-5,-2),(-2.5,3),(1,-2),(3.5,2.5),(7,-3)]
+    f: FunctionInterpolationApproximator = None
+    return sp,ep,racines,extrema,f
+
+
+def launch_approximate(racines,extrema,sp,ep):
+    racines = list(map(lambda x:float(x),racines))
+    sp = [float(p) for p in str(sp).strip("()").split(",")]
+    ep = [float(p) for p in str(ep).strip("()").split(",")]
+    domaine = [sp[0],ep[0]]
+    extrema = [sp] + [[float(p) for p in str(e).strip("()").split(",")] for e in extrema] + [ep]
+    f = FunctionInterpolationApproximator(racines, extrema, domaine)
+    f.approximate()
+    return f
+
+def close_session(uid):
+    global session_alive
+    while True:
+        time.sleep(15)
+        if session_alive[uid] == 0:
+            session_instances[uid].stop()
+            break
+        session_alive[uid] = 0
+
+@app.before_request
+def before_request():
+    global session_alive
+    if session.get('uid'):
+        session_alive[session['uid']] = 1
+
+@app.route('/')
+def home():
+    sp,ep,racines,extrema,f = setDefaultValues()
+    if not session.get('uid'):
+        session['uid'] = uuid.uuid4()
+        session_alive.update({session['uid']: 0})
+        t = Thread(target=close_session, args=(session['uid'],))
+        t.start()
+    f = launch_approximate(racines,extrema,sp,ep)
+    session_instances.update({session['uid']: f})
+    latex = f.axisText()
+    geogebra = f.geogebraText()
+    return render_template('FunctionApproximator.html',racines=racines,extrema=extrema,sp=sp,ep=ep,latex=latex,geogebra=geogebra)
+
+@app.route('/approx', methods=['GET', 'POST'])
+def approximate():
+    latex = None
+    geogebra = None
+    f = session_instances[session['uid']]
+    if request.method == "POST":
+        result = request.form
+        racines = []
+        extrema = []
+        for key,value in result.items():
+            if key.startswith("racine"):
+                racines.append(value)
+            if key.startswith("extremum"):
+                extrema.append(value)
+            if key == "sp":
+                sp = value
+            if key == "ep":
+                ep = value
+        for i in range(len(racines)):
+            if f"remove racine {i}" in result:
+                del racines[i]
+                f = launch_approximate(racines,extrema,sp,ep)
+                latex = f.axisText()
+                geogebra = f.geogebraText()
+        for i in range(len(extrema)):
+            if f"remove extremum {i}" in result:
+                del extrema[i]
+                f = launch_approximate(racines,extrema,sp,ep)
+                latex = f.axisText()
+                geogebra = f.geogebraText()
+        if "add racine" in result:
+            racines.append(0)
+            latex = None
+            geogebra = None
+        if "add extremum" in result:
+            extrema.append((0,0))
+            latex = None
+            geogebra = None
+        if "clear racines" in result:
+            racines = []
+        if "clear extrema" in result:
+            extrema = []
+        if "approximate" in result:
+            f = launch_approximate(racines,extrema,sp,ep)
+            latex = f.axisText()
+            geogebra = f.geogebraText()
+
+        if not session.get('uid'):
+            session['uid'] = uuid.uuid4()
+            session_alive.update({session['uid']: 0})
+            t = Thread(target=close_session, args=(session['uid'],))
+            t.start()
+        f = launch_approximate(racines, extrema, sp, ep)
+        session_instances.update({session['uid']: f})
+
+    return render_template('FunctionApproximator.html',racines=racines,extrema=extrema,sp=sp,ep=ep,latex=latex,geogebra=geogebra)
+
+@app.route('/plot.png')
+def plot_png():
+    f = session_instances[session['uid']]
+    fig = create_figure(f)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure(f):
+    if f == None:
+        return None
+
+    fig = Figure(figsize=(f.max_x-f.min_x,f.max_y-f.min_y),dpi=300)
+    axis = fig.add_subplot(1, 1, 1)
+    # set up a plot
+    ax1 = axis
+
+    # spine placement data centered
+    ax1.spines['left'].set_position(('data', 0.0))
+    ax1.spines['bottom'].set_position(('data', 0.0))
+    ax1.spines['right'].set_color('none')
+    ax1.spines['top'].set_color('none')
+
+    ax1.set_aspect("equal")
+
+    ax1.grid(True)
+    tf = f
+    miny = f.min_y
+    maxy = f.max_y
+    ax1.set_ylim(1.2*f.min_y, 1.2*f.max_y)
+    ax1.set_xlim(f.min_x, f.max_x)
+    ax1.set_xticks(np.arange(np.ceil(0.95*f.min_x), np.floor(0.95*f.max_x)+1, 1))
+    ax1.set_yticks(np.arange(np.ceil(1.2*f.min_y), np.floor(1.2*f.max_y)+1, 1))
+    xs=f.xs
+    ys=f.ys
+
+    axis.plot(xs, ys)
+    fig.tight_layout()
+
+    return fig
